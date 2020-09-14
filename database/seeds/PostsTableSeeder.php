a21050eff54c010a28a15f513f19e1e1c23d6dcb
Sara <saracorsob@gmail.com>
2020-09-14 17:36:38 +0200

    Creazione migration + seeder

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+      for ($i=0; $i < 10; $i++) {
+        $new_post = new Post();
+        $new_post->title = $faker->sentence();
+        $new_post->content = $faker->paragraph();
+        $new_post->user_id = 1;
+      }
+    }
+}
